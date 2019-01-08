4ab5f4b1b54081fa2c11f60e214cbd7c8275c8cc
Luis Eduardo Jiménez Robles <luisejimenezrobles@gmail.com>
2019-01-07 22:41:53 -0600

    express imported in app.ts and routes for crm created at crmroutes.ts file

---
+++ lib/app.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Routes } from './routes/crmRoutes';
+
+class App {
+    public app: express.Application
+    public routePRV: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        this.config;
+        this.routePRV.Routes(this.app);
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+}
+
+export default new App().app
---
+++ lib/server.ts
@@ -0,0 +1,6 @@
+import app from './app';
+const PORT = 3000;
+
+app.listen( 3000, () => {
+    console.log('Express server is listening at port: ' + PORT );
+})
