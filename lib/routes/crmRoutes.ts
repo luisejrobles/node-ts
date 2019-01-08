03a77fb7112bc78329d19bd7bb2402e5fb0918ad
Luis Eduardo Jiménez Robles <luisejimenezrobles@gmail.com>
2019-01-07 22:47:13 -0600

    PUT, GET, POST, DELETE requests added

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -1,12 +1,52 @@
-import {  Request, Response } from 'express';
+import { Request, Response } from 'express';
 
 export class Routes {
     public Routes(app): void {
         app.route('/')
-        .get( (req: Request, res: Response) => {
-            res.status(200).send({
-                message: 'GET request successfull!'
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'GET request successfull!'
+                })
             })
-        })
+
+        // Contacts
+        app.route('/contact')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'GET contact request successful!'
+                })
+            })
+
+        // Get a specific contact
+        app.route('/contact/:id')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "GET specific contact successful!"
+                })
+            })
+
+            // POST endpoints
+            // Create a new user
+            .post((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "POST new contact successfull!"
+                })
+            })
+
+            // PUT
+            // Updating a contact
+            .put((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'PUT request successfull!'
+                })
+            })
+
+            // DELETE
+            // Deleting a user
+            .delete((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'DELETE request successfull!'
+                })
+            })
     }
 }
